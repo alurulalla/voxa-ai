aadb5b455f35b01a1070dda1cdb8b1811cb07911
Aluru Murali Krishna <alurumuralikrishna9.com>
2026-03-04 12:41:24 +0530

    feat: Added turbo repo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,16 +1,18 @@
 import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World Web</h1>
+        <h1 className="text-2xl font-bold">Hello World Widget Updated</h1>
         <div className="flex gap-2">
           <Button>Button</Button>
           <Button variant="outline">Outline</Button>
         </div>
-        {add(9, 9)}
+        {add(18, 18)}
+        <Input />
       </div>
     </div>
   );
