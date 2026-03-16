5f03cc279050c952ddc49b131d4cca0469f78805
Aluru Murali Krishna <alurumuralikrishna9.com>
2026-03-16 20:05:16 +0530

    Added Vapi

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,19 +1,27 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
-import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
+import { useVapi } from "@/modules/widget/hooks/use-vapi";
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
-  const addUser = useMutation(api.users.add);
+  const {
+    isConnected,
+    isConnecting,
+    isSpeaking,
+    startCall,
+    endCall,
+    transcript,
+  } = useVapi();
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <p>apps/widget</p>
-      <div className="max-w-sm w-full mx-auto">
-        {JSON.stringify(users, null, 2)}
-      </div>
-      <Button onClick={() => addUser()}>Add</Button>
+      <Button onClick={() => startCall()}>Start Call</Button>
+      <Button onClick={() => endCall()} variant="destructive">
+        End Call
+      </Button>
+      <p>isConnected: {isConnected}</p>
+      <p>isConnecting: {isConnecting}</p>
+      <p>isSpeaking: {isSpeaking}</p>
+      <p>{JSON.stringify(transcript, null, 2)}</p>
     </div>
   );
 }
