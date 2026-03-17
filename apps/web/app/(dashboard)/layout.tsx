b4b54a0373e38d7e794429c5be5dbda06bb63b1f
Aluru Murali Krishna <alurumuralikrishna9.com>
2026-03-17 11:45:34 +0530

    Added sidebar dashboard

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,13 +1,10 @@
 import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
 import { OrganizationGuard } from "@/modules/auth/ui/components/organization-guard";
+import { DashboardLayout } from "@/modules/dashboard/ui/layouts/dashboard-layout";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   console.log("in dashboard");
-  return (
-    <AuthGuard>
-      <OrganizationGuard>{children}</OrganizationGuard>
-    </AuthGuard>
-  );
+  return <DashboardLayout>{children}</DashboardLayout>;
 };
 
 export default Layout;
